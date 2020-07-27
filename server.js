df76d7411fcfa33b0c63bf420c1f79485b068a2f
Jake Miller <jake.e.miller@gmail.com>
2020-07-27 03:44:24 +0000

    Cleanup and format

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var multer = require('multer'); // v1.0.5
+var multer = require('multer');
 var app = express();
 var upload = multer();
 app.use(express.static('views'));
@@ -11,16 +11,15 @@ app.use(bodyParser.json());
 
 var config = require('./config')
 
-app.post("/", upload.array(),function (req, response) {
-
-  console.log( req.body );
-  console.log ( req.body.value2);
+app.post('/', upload.array(), function(req, response) {
+  console.log(req.body);
+  console.log(req.body.value2);
   var playerId = config.playerMapping[req.body.value2];
   var server = config.serverMapping[req.body.value1];
   var gamename = req.body.value1;
-  var mention = "";
+  var mention = '';
 
-  console.log( playerId);
+  console.log(playerId);
   var turnNumber = req.body.value3;
 
   if (!server) {
@@ -28,42 +27,39 @@ app.post("/", upload.array(),function (req, response) {
   }
 
   if (playerId) {
-    mention = "<@" + playerId + ">";
-  } else  {
-    mention = "@" + req.body.value2;
+    mention = '<@' + playerId + '>';
+  } else {
+    mention = '@' + req.body.value2;
   }
 
   if (server) {
-    var content = "Hey "+ mention + ", it's time to take your turn #" + turnNumber + " in '" + gamename +"'!";
-    sendMessage( server, content);
-    console.log("Done triggering.");
+    var content = 'Hey ' + mention + ', it\'s time to take your turn #' +
+        turnNumber + ' in \'' + gamename + '\'!';
+    sendMessage(server, content);
+    console.log('Done triggering.');
   } else {
-    var content = "Error in data, missing game '" + req.body.value1 + "'?";
-    sendMessage( config.debugserver,content );
-    console.log( content );
+    var content = 'Error in data, missing game \'' + req.body.value1 + '\'?';
+    sendMessage(config.debugserver, content);
+    console.log(content);
   }
 
   response.end();
 });
 
-function sendMessage( server, content )
-{
-  request({
-    uri: server,
-    body: { "content":content},
-    json: true,
-    method: 'POST'
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body); // Show the response from Habitica
-    } else {
-      console.log(response.statusCode);
-      console.log(body);
-    }
-  });
+function sendMessage(server, content) {
+  request(
+      {uri: server, body: {'content': content}, json: true, method: 'POST'},
+      function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+        } else {
+          console.log(response.statusCode);
+          console.log(body);
+        }
+      });
 }
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
