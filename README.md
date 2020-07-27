13cf382de3db69af0e14a06f9b006ab7615c9a11
Jake Miller <jake.e.miller@gmail.com>
2020-07-27 03:39:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Civ a Little - Civ 6 Notifier and Webviewer
+
+This application does as so many have done before it but decides to extend it further. The Civ 6 webhook does a POST with the infamous JSON, e.g. `'{"value1":"TestGame","value2":"TestUser","value3":"1"}'`. This application intends to record that data and expose it in different human readable formats as well as use it for statistical analysis.
+
+Work in progress.
---
+++ server.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var multer = require('multer'); // v1.0.5
+var app = express();
+var upload = multer();
+app.use(express.static('views'));
+app.use(bodyParser.json());
+
+var config = require('./config')
+
+app.post("/", upload.array(),function (req, response) {
+
+  console.log( req.body );
+  console.log ( req.body.value2);
+  var playerId = config.playerMapping[req.body.value2];
+  var server = config.serverMapping[req.body.value1];
+  var gamename = req.body.value1;
+  var mention = "";
+
+  console.log( playerId);
+  var turnNumber = req.body.value3;
+
+  if (!server) {
+    server = config.defaultServer;
+  }
+
+  if (playerId) {
+    mention = "<@" + playerId + ">";
+  } else  {
+    mention = "@" + req.body.value2;
+  }
+
+  if (server) {
+    var content = "Hey "+ mention + ", it's time to take your turn #" + turnNumber + " in '" + gamename +"'!";
+    sendMessage( server, content);
+    console.log("Done triggering.");
+  } else {
+    var content = "Error in data, missing game '" + req.body.value1 + "'?";
+    sendMessage( config.debugserver,content );
+    console.log( content );
+  }
+
+  response.end();
+});
+
+function sendMessage( server, content )
+{
+  request({
+    uri: server,
+    body: { "content":content},
+    json: true,
+    method: 'POST'
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body); // Show the response from Habitica
+    } else {
+      console.log(response.statusCode);
+      console.log(body);
+    }
+  });
+}
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
